ed23aeaee951a57b503f75274702c2dd9e2ff7d8
Ahmadhyari <ahmadhyari991998@gmail.com>
2022-02-01 02:13:22 +0200

    add get

--- server.js
+++ server.js
@@ -2,18 +2,27 @@
 const express = require("express");
 const app = express();
 const appData = require(`./MovieData/data.json`);
+const axios = require("axios");
+const dotenv = require("dotenv");
+dotenv.config();
+const APIKEY = process.env.APIKEY;
+const PORT = process.env.PORT;
 
 
-app.get(`/`, review);
 
-function Movie(title, poster_path, overview) {
+function Movie(id, title, release_date, poster_path, overview) {
+    this.id = id;
     this.title = title;
+    this.release_date = release_date
     this.poster_path = poster_path;
     this.overview = overview;
 
 }
 
+app.get(`/`, review);
+
 function review(req, res) {
+
 
     let movieInfo = new Movie(appData.title, appData.poster_path, appData.overview);
 
@@ -31,7 +40,77 @@ function favoritePage(req, res) {
 }
 
 
+app.get("/trending", getTrendingHandler)
 
-app.listen(3198, () => {
-    console.log("im listen");
+function getTrendingHandler(req, res) {
+    let trendMovie = [];
+    axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}&language=en-US`).then(value => {
+        value.data.results.forEach(element => {
+            let oneMovie = new Movie(element.id, element.title, element.release_date, element.poster_path, element.overview);
+            trendMovie.push(oneMovie);
+        })
+        
+        return res.status(200).json(trendMovie);
+    }).catch((error) => {
+        errorHandler(error, req, res)
+    })
+
+}
+
+app.get("/search", searchMoviesHandler),
+
+    function searchMoviesHandler(req, res) {
+        let searchQuery = req.query.search;
+        let selectedMovie = [];
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&language=en-US&query=${searchQuery}`).then(value => {
+            value.data.results.forEach(element => {
+                selectedMovie.push(element);
+            })
+            return res.status(200).json(selectedMovie);
+        }).catch((error) => {
+            errorHandler(error, req, res)
+        })
+    }
+
+
+
+app.get("/configuration" , conf),
+function conf (req ,res){
+   
+    axios.get(`https://api.themoviedb.org/3/configuration?api_key=${APIKEY}&language=en-US`).then (value => {
+       let conf= value.data.images;
+       return res.status(200).json(conf);
+    }).catch((error) => {
+        errorHandler(error, req, res)
+    })
+}
+
+
+
+
+
+
+
+
+app.use(errorHandler);
+
+function errorHandler(error, req, res) {
+    const err = {
+        status: 500,
+        Aleart: Error
+    }
+
+    res.status(500).send(err);
+}
+
+
+
+app.use("*", notFountHandler);
+function notFountHandler(req, res) {
+    res.status(404).send("Something Error -Not found-");
+}
+
+
+app.listen(PORT, () => {
+    console.log(`im listen to ${PORT}`);
 });
